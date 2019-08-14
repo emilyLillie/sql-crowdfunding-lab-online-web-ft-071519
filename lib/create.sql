2ce1cb5796174dfdb2ab65d861ab5ec3d8862945
Emily Lillie <emilylillie3@gmail.com>
2019-08-14 23:19:38 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,5 +2,16 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER); 
+  
+  CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date INTEGER,
+  end_date INTEGER);
 
+
+
+projects (id, title, category, funding_goal, start_date, end_date)
 
