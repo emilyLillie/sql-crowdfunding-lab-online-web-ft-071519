0bcb061f79a50fb387cf09b52d79524576d213d6
Emily Lillie <emilylillie3@gmail.com>
2019-08-14 23:37:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,6 @@
 
 
-INSERT INTO users (name, age) VALUES ('Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
-(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
+INSERT INTO users (name, age) VALUES ('Ena', 24), (20, 'Katie', 24);
 
 INSERT INTO users (name, age) VALUES ('Finnebar', 17);
 INSERT INTO users (name, age) VALUES ('Bear', 6);
@@ -14,6 +13,15 @@ INSERT INTO users (name, age) VALUES ('Victoria', 23);
 INSERT INTO users (name, age) VALUES ('Franz', 100);
 INSERT INTO users (name, age) VALUES ('Hermione', 30);
 INSERT INTO users (name, age) VALUES ('Voldemort', 90);
+INSERT INTO users (name, age) VALUES ('Marisa', 24);
+INSERT INTO users (name, age) VALUES ('Swizzle', 4);
+INSERT INTO users (name, age) VALUES ('Sirius', 36);
+INSERT INTO users (name, age) VALUES ('Albus', 113);
+INSERT INTO users (name, age) VALUES ('Squid', 5);
+INSERT INTO users (name, age) VALUES ('Whale', 6);
+INSERT INTO users (name, age) VALUES ('Pacha', 5);
+INSERT INTO users (name, age) VALUES ('Ena', 24);
+
 
 
 
