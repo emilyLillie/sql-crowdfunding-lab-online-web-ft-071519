8dcf67c7af87b78e0716fc917e4ae978e37632dc
Emily Lillie <emilylillie3@gmail.com>
2019-08-14 23:33:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,6 @@
 
 
-INSERT INTO users (name, age) VALUES ('Franz', 100), (10, 'Hermione', 30),
-(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+INSERT INTO users (name, age) VALUES ('Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
 (17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
 INSERT INTO users (name, age) VALUES ('Finnebar', 17);
@@ -12,6 +11,12 @@ INSERT INTO users (name, age) VALUES ('Amanda', 24);
 INSERT INTO users (name, age) VALUES ('Sophie', 24);
 INSERT INTO users (name, age) VALUES ('Rosey', 9);
 INSERT INTO users (name, age) VALUES ('Victoria', 23);
+INSERT INTO users (name, age) VALUES ('Franz', 100);
+INSERT INTO users (name, age) VALUES ('Hermione', 30);
+INSERT INTO users (name, age) VALUES ('Voldemort', 90);
+
+
+
 
 
 
